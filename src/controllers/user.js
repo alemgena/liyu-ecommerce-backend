0411e66d0e7b5fb682ec3808fbc882562ef27613
alemgena <alemgenateferi1@gmil.com>
2022-12-29 15:10:31 +0300

    remove duplicate code

--- src/controllers/user.js
+++
@@ -1,8 +0,0 @@
-const httpStatus = require("http-status");
-const catchAsync = require("../utils/catchAsync");
-const { user} = require("../services");
-exports.changeUserPasswird = catchAsync(async (req, res) => {
-  const data = await user.changePassword({ ...req.body });
-  res.status(200).send( {data:data} );
-});
-  
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,8 @@
 const auth = require("./auth");
 const product=require("./Product")
-const user=require('./user')
 const Routers = {
   auth,
   product,
-  user
+  
 };
 module.exports = Routers;
