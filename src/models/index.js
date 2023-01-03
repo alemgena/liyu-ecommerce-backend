1ffd2ee6e9a5b40cf709d64933085b8805072f57
yonas kemon <yonas@mohastech.com>
2023-01-03 23:39:47 +0300

    EB-54 implmented get sub categories of category feature

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
 module.exports.User = require("./user");
 module.exports.Product = require("./product");
 module.exports.Category = require("./category");
-module.exports.Subcategory = require("./subCategory");
+module.exports.subCategory = require("./subCategory");
 module.exports.ProductImage = require("./productImages");
