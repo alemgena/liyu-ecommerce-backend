72692de1a6462710e6448be786bd58cac5139d92
yonas kemon <yonas@mohastech.com>
2023-01-06 17:10:43 +0300

    update error response and error wrapper

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
-const httpStatus = require('http-status');
-const config = require('../config/config');
-const logger = require('../config/logger');
-const ApiError = require('../utils/ApiError');
+const mongoose = require("mongoose");
+const httpStatus = require("http-status");
+const config = require("../config/config");
+const logger = require("../config/logger");
+const ApiError = require("../utils/ApiError");
+const Joi = require("joi");
 
 const errorConverter = (err, req, res, next) => {
   let error = err;
   if (!(error instanceof ApiError)) {
     const statusCode =
-      error.statusCode || error instanceof mongoose.Error ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+      error.statusCode || error instanceof mongoose.Error
+        ? httpStatus.BAD_REQUEST
+        : httpStatus.INTERNAL_SERVER_ERROR;
     const message = error.message || httpStatus[statusCode];
-    error = new ApiError(statusCode, message, false, err.stack);
+    error = new ApiError(statusCode, message, err, false);
   }
   next(error);
 };
@@ -18,7 +21,7 @@ const errorConverter = (err, req, res, next) => {
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
-  if (config.env === 'production' && !err.isOperational) {
+  if (config.env === "production" && !err.isOperational) {
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
   }
@@ -28,14 +31,22 @@ const errorHandler = (err, req, res, next) => {
   const response = {
     code: statusCode,
     message,
-    ...(config.env === 'development' && { stack: err.stack }),
+    ...(config.env === "development" && { stack: err.error.stack }),
+    field_error:
+      err.error instanceof Joi.ValidationError
+        ? err.error.details.map((details) => {
+            return {
+              name: details.context.label,
+              message: details.message,
+            };
+          })
+        : [],
   };
 
-  if (config.env === 'development') {
+  if (config.env === "development") {
     logger.error(err);
   }
-
-  res.status(statusCode).send(response);
+  res.status(statusCode).json({ success: false, error: response });
 };
 
 module.exports = {
