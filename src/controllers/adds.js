d8a0c1f3c5a9f532e1fbcbe4dc6511aba2e4d330
alemgena <alemgenateferi1@gmil.com>
2023-01-13 21:57:18 +0300

    add response message

--- src/controllers/adds.js
+++ src/controllers/adds.js
@@ -1,7 +1,12 @@
 const httpStatus = require("http-status");
 const catchAsync = require("../utils/catchAsync");
-const {adds}=require('../services')
+const {adds}=require('../services');
+const SuccessResponse = require("../utils/successResponse");
 exports.add = catchAsync(async (req, res) => {
     const data = await adds.add({ ...req.body });
-    res.status(httpStatus.CREATED).send({ data });
+    res
+    .status(httpStatus.OK)
+    .send(
+      new SuccessResponse(httpStatus.CREATED, "", data)
+    );
   });
